0c7d1a8ef0104fdbf2e42f0122c23ec269372673
Andrey Dobrynin <andrdobrynin@gmail.com>
2018-11-05 02:51:51 +0700

    Update bash script for deploy

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+php7_2=/usr/local/php/php-7.2/bin/php
+composer="$php7_2 /home/$USER/.composer/composer.phar"
+
 pwd
 echo 'change dir'
 cd dobrynin-andrey.ru/p-256/
@@ -9,7 +12,8 @@ echo 'fetch and merge'
 git pull bitbucket master
 
 echo 'composer update and clean cache'
-/usr/local/php/php-7.2/bin/php /home/$USER/.composer/composer.phar install
+${composer} install
+${php7_2} bin/console cache:clear --env=prod --no-debug
 
 echo "Successfully deploy"
 exit 0
